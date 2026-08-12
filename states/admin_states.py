ccac97a5c96208537e49fbdc04887ab549ff043b
Jabborovmuhammadyusuf <muhammadyusufsevgi@gmail.com>
2026-08-12 09:10:57 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -2,3 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AdminBroadcast(StatesGroup):
     waiting_for_message = State()
+
+class AdminPremium(StatesGroup):
+    waiting_for_user_id = State()
+    waiting_for_days = State()
