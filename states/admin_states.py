119d1aef4067d137b5bf49c70a9997ca22c593d8
Jabborovmuhammadyusuf <muhammadyusufsevgi@gmail.com>
2026-08-12 10:05:57 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -6,3 +6,11 @@ class AdminBroadcast(StatesGroup):
 class AdminPremium(StatesGroup):
     waiting_for_user_id = State()
     waiting_for_days = State()
+
+class AdminTariff(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_days = State()
+    waiting_for_price = State()
+
+class AdminCard(StatesGroup):
+    waiting_for_card_number = State()
